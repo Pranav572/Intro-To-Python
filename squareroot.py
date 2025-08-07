3c65ebc05f8cd326efb8ed90d8a260d4c684c6e7
Pranav572 <sahananand@gmail.com>
2025-08-07 12:07:12 +0400

    Add files via upload

---
+++ squareroot.py
@@ -0,0 +1,7 @@
+print("enter the two numbers you would like to perform square root")
+number1=int(input("First Number="))
+number2=int(input("Second Number="))
+
+sqr=(number1**number2)
+print("The Square Root=")
+print(sqr)
