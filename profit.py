7f1a97216b9414f32acb3622e28c401ff9a56f13
Pranav572 <sahananand@gmail.com>
2025-08-08 14:03:32 +0400

    Add files via upload

---
+++ profit.py
@@ -0,0 +1,7 @@
+actualamount=float(input("Enter The Amount:"))
+salesamount=float(input("Enter The Sales Amount"))
+if salesamount>actualamount:
+    print("You Have Profited:",salesamount-actualamount)
+
+else:
+    print("loss")
