e0b0f5f3e25c41820ac7f29d9d0e3e3b426d9783
Pranav572 <sahananand@gmail.com>
2025-08-11 13:58:57 +0400

    Add files via upload

---
+++ andor.py
@@ -0,0 +1,18 @@
+a=int(input("Enter Your Age:"))
+b=str(input("Enter Your Gender:"))
+
+if a>18 and b=="male":
+    print("You Are Edigble For This Job")
+else:
+    print("You Are Not Edigble For This Job")
+
+
+c=18
+d=22
+
+if c>d or d<c:
+    print("This Is An Example of OR")
+
+else:
+    print("False")
+
