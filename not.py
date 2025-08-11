9a83b2431dbdd64ff0bc823564ff81781b1291fd
Pranav572 <sahananand@gmail.com>
2025-08-11 14:05:59 +0400

    Add files via upload

---
+++ not.py
@@ -0,0 +1,11 @@
+a=7
+b=21
+c=44
+if b!=a:
+    print("A is not equal to B")
+
+
+age=int(input("Enter Age :"))
+siblingage=int(input("Enter Ur Sibling Age:"))
+if age!=siblingage:
+    print("You Are Younger Than Your Sibling")
